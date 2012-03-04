489e3690cf46f91d456a7235368062e5e5b15fad
Yoan Blanc <yoan@dosimple.ch>
2012-03-04 12:32:45 +0100

    Creating rails application (no config, nothing)

---
+++ nba/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nba::Application.config.secret_token = 'd1c6a196b4447373320405e72fea4798e5d8cf8edeff0b892cb82192c61bdeb6eb9fe1f96c2fc63914b035d84f91c1cacf2379311a30bfdf6115a2fdbc72ad37'
