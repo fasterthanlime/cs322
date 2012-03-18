1a9bacafaffffa22d5e028a5e506eb8194b3ddda
Yoan Blanc <yoan@dosimple.ch>
2012-03-18 20:28:37 +0100

    Adding more tables to the SQL schema. (#3)

---
+++ documents/sql/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO "leagues" ("id", "name") VALUES ("leagues_seq".NEXTVAL, 'NBA');
+INSERT INTO "leagues" ("id", "name") VALUES ("leagues_seq".NEXTVAL, 'ABA');
+
+INSERT INTO "conferences" ("id", "name") VALUES ("conferences_seq".NEXTVAL, 'Eastern');
+INSERT INTO "conferences" ("id", "name") VALUES ("conferences_seq".NEXTVAL, 'Western');
