723a8c7083cc2090f56f3a1d1f0b24a39129a349
Yoan Blanc <yoan@dosimple.ch>
2012-03-19 22:02:44 +0100

    Stuff that works, leagues and conferences (#8)

--- nba/app/controllers/welcome_controller.rb
+++ nba/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @people = Person.find(:all)
+    @leagues = League.find(:all)
+    @conferences = Conference.find(:all)
   end
 end
---
+++ nba/app/models/conference.rb
@@ -0,0 +1,3 @@
+class Conference < ActiveRecord::Base
+  attr_accessible :name
+end
