3ad681b547a02b5fa4722d9c043226f08413c1ed
Amos Wenger <amos@official.fm>
2012-04-27 14:11:26 +0200

    Better script

--- queries/launch-query.sh
+++ queries/launch-query.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 query=$1;
+SQL="sqlplus DB2012_G06/DB2012_G06"
 
-sqlplus DB2012_G06/DB2012_G06 < basic_${query}.sql
-echo "SELECT * FROM query_${query};" | sqlplus DB2012_G06/DB2012_G06
+${SQL} < basic_${query}.sql
+echo "SELECT * FROM query_${query};" | ${SQL} 
