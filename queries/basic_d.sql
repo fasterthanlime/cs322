1f049d4827597c96dc1ca9a2d2f398b22e199b51
Amos Wenger <amos@official.fm>
2012-04-27 02:30:33 +0200

    Welp, that should do it... but it returns empty for now :(

--- queries/basic_d.sql
+++ queries/basic_d.sql
@@ -1,3 +1,21 @@
 -- Print the names of coaches who participated in both leagues (NBA and ABA).
 
-TODO
+CREATE OR REPLACE VIEW nba_coaches AS SELECT DISTINCT p.id
+FROM people p
+JOIN coaches c ON c.person_id = p.id
+    JOIN coaches_teams ct ON ct.coach_id = c.id
+    JOIN team_seasons ts ON ts.team_id = ct.team_id AND ts.year = ct.year
+        JOIN leagues l ON ts.league_id = l.id
+WHERE l.name = 'NBA';
+
+CREATE OR REPLACE VIEW aba_coaches AS SELECT DISTINCT p.id
+FROM people p
+JOIN coaches c ON c.person_id = p.id
+    JOIN coaches_teams ct ON ct.coach_id = c.id
+    JOIN team_seasons ts ON ts.team_id = ct.team_id AND ts.year = ct.year
+        JOIN leagues l ON ts.league_id = l.id
+WHERE l.name = 'ABA';
+
+SELECT * FROM nba_coaches
+  INTERSECT
+SELECT * FROM aba_coaches;
