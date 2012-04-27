a0065d61a284acf575f9bd64fa41db6d9111ff7f
Amos Wenger <amos@official.fm>
2012-04-27 16:31:39 +0200

    Worked a bit on request f

--- queries/basic_f.sql
+++ queries/basic_f.sql
@@ -1,8 +1,21 @@
 -- Print the names of oldest and youngest player that have participated in the
 -- playoffs for each season.
 
-CREATE OR REPLACE VIEW query_f AS
-SELECT * FROM teams;
+CREATE OR REPLACE VIEW youngest_players AS
+SELECT id youngest_id, firstname youngest_firstname, lastname youngest_lastname, birthdate youngest_birthdate, year
+FROM (
+    SELECT id, lastname, firstname, year, birthdate, RANK() OVER (PARTITION BY year ORDER BY birthdate DESC) r
+    FROM (
+        SELECT pl.id, p.lastname, p.firstname, psea.year, pl.birthdate
+        FROM players pl
+        JOIN player_seasons psea ON psea.player_id = pl.id
+          JOIN player_stats psta ON psta.player_season_id = psea.id
+            JOIN player_season_types psty ON psty.name = 'Playoff' AND psta.player_season_type_id = psty.id
+        JOIN people p ON p.id = pl.person_id
+    )
+)
+WHERE r = 1;
 
--- TODO
+CREATE OR REPLACE VIEW query_f AS
+SELECT * FROM youngest_players;
 
