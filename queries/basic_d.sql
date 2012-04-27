be7c0a970943f25077aecf4f18c58a1f8e0160db
Amos Wenger <amos@official.fm>
2012-04-27 13:56:12 +0200

    Fun with querie

--- queries/basic_d.sql
+++ queries/basic_d.sql
@@ -3,19 +3,19 @@
 CREATE OR REPLACE VIEW nba_coaches AS SELECT DISTINCT p.id
 FROM people p
 JOIN coaches c ON c.person_id = p.id
-    JOIN coaches_teams ct ON ct.coach_id = c.id
+  JOIN coaches_teams ct ON ct.coach_id = c.id
     JOIN team_seasons ts ON ts.team_id = ct.team_id AND ts.year = ct.year
-        JOIN leagues l ON ts.league_id = l.id
+      JOIN leagues l ON ts.league_id = l.id
 WHERE l.name = 'NBA';
 
 CREATE OR REPLACE VIEW aba_coaches AS SELECT DISTINCT p.id
 FROM people p
 JOIN coaches c ON c.person_id = p.id
-    JOIN coaches_teams ct ON ct.coach_id = c.id
+  JOIN coaches_teams ct ON ct.coach_id = c.id
     JOIN team_seasons ts ON ts.team_id = ct.team_id AND ts.year = ct.year
-        JOIN leagues l ON ts.league_id = l.id
+      JOIN leagues l ON ts.league_id = l.id
 WHERE l.name = 'ABA';
 
-SELECT * FROM nba_coaches
-  INTERSECT
+--SELECT * FROM nba_coaches;
+  --INTERSECT
 SELECT * FROM aba_coaches;
--- queries/basic_e.sql
+++ queries/basic_e.sql
@@ -1,3 +1,20 @@
 -- Compute the highest scoring and lowest scoring player for each season.
 
-TODO
+CREATE OR REPLACE VIEW best_player AS SELECT id, counter
+FROM (
+    SELECT id, counter, RANK() OVER (ORDER BY counter DESC) r
+    FROM (
+        SELECT pl.id, SUM(stat.pts) counter
+        FROM players pl
+        JOIN player_seasons psea ON psea.player_id = pl.id
+         JOIN player_stats psta ON psta.player_season_id = psea.id
+          JOIN stats stat ON stat.id = psta.stat_id
+        GROUP BY pl.id
+    )
+)
+WHERE r = 1;
+
+SELECT firstname, lastname
+FROM best_player bp
+JOIN players pl ON pl.id = bp.id
+JOIN people p ON p.id = pl.person_id;
