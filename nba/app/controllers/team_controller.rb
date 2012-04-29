a65860d7ce284e41704085d58a9ee6e7d5b777e4
Yoan Blanc <yoan@dosimple.ch>
2012-04-29 16:13:02 +0200

    View work on the coach, team. #8

--- nba/app/controllers/team_controller.rb
+++ nba/app/controllers/team_controller.rb
@@ -1,6 +1,11 @@
 class TeamController < ApplicationController
   def index
-    @teams = Team.find(:all)
+    @nba = League.find_by_name('NBA')
+    @aba = League.find_by_name('ABA')
+    @teams = {
+      "NBA" => Team.find(:all, :conditions => {:league_id => @nba.id}, :order => 'trigram'),
+      "ABA" => Team.find(:all, :conditions => {:league_id => @aba.id}, :order => 'trigram')
+    }
   end
 
   def view
