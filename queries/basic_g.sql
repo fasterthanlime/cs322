c7c1150ce3fb31c11c73313bf7055018be2bc6d7
Amos Wenger <amos@official.fm>
2012-05-10 22:49:30 +0200

    Implement query G. Closes #16.

--- queries/basic_g.sql
+++ queries/basic_g.sql
@@ -1,4 +1,14 @@
 -- List the name of the schools according to the number of players they sent to
 -- the NBA. Sort them in descending order by number of drafted players.
 
+SELECT
+    l.name, COUNT(l.id) counter
+FROM
+    locations l
+    JOIN drafts d ON d.location_id = l.id
+GROUP BY
+    l.name
+ORDER BY
+    counter DESC;
+
 -- STUB
