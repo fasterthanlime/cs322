38beef45dcf49447ad4b7f4faf2ae001827e4434
Amos Wenger <amos@official.fm>
2012-05-11 01:06:38 +0200

    I think I'm onto something with query I ;)

--- queries/basic_i.sql
+++ queries/basic_i.sql
@@ -2,7 +2,7 @@
 -- with more than 'XXX' season career wins and more than 'YYY' win percentage,
 -- in each season they coached.
 --
--- ('XXX' and 'YYY' are parameters. Try with combinations:
+-- 'XXX' and 'YYY' are parameters. Try with combinations:
 -- XXX   YYY
 -- 1000  70%
 -- 1000  60%
@@ -12,5 +12,38 @@
 -- 700   45%
 --
 -- Sort the result by year in ascending order
+-- 
+-- CREATE OR REPLACE VIEW player_avg AS
+-- SELECT
+--     AVG(pl.weight) avg_weight, AVG(pl.height) avg_height, AVG(ROUND((SYSDATE - pl.birthdate)/365.24,0)) avg_age
+-- FROM
+--     players pl
+-- END
 
--- STUB
+-- A few utility views
+
+CREATE OR REPLACE VIEW coach_seasons_percentage AS
+    SELECT
+        coach_id, season_win, season_loss, 100 * (season_win / (season_win + season_loss)) win_percentage
+    FROM
+        coach_seasons cs
+;
+
+CREATE OR REPLACE VIEW coach_seasons_career AS
+    SELECT
+        coach_id, SUM(season_win) career_wins
+    FROM
+        coach_seasons cs
+    GROUP BY
+        coach_id
+;
+
+SELECT
+    *
+FROM
+    coach_seasons_percentage cp
+    JOIN coach_seasons_career cc ON cp.coach_id = cc.coach_id
+ORDER BY
+    career_wins
+;
+
