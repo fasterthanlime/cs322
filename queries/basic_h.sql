a3beed180f97911028c31f45e5824572f8f306f1
Amos Wenger <amos@official.fm>
2012-05-10 22:56:44 +0200

    Implement query H. Closes #17

--- queries/basic_h.sql
+++ queries/basic_h.sql
@@ -1,4 +1,18 @@
 -- List the name of the schools according to the number of players they sent to
 -- the ABA. Sort them in descending order by number of drafted players.
 
+SELECT
+    l.name, COUNT(l.id) counter
+FROM
+    locations l
+    JOIN drafts d   ON d.location_id = l.id
+    JOIN teams t    ON t.id = d.team_id
+    JOIN leagues le ON le.id = t.league_id
+WHERE
+    le.name = 'ABA'
+GROUP BY
+    l.name
+ORDER BY
+    counter DESC;
+
 -- STUB
