21d88783cc3604925c7cb573075075ec8c88da09
Amos Wenger <amos@official.fm>
2012-05-23 01:56:25 +0200

    Implemented query T... although having 5796 rows is kinda fishy, if you want my opinion. Closes #29

--- queries/basic_t.sql
+++ queries/basic_t.sql
@@ -1,3 +1,20 @@
 -- List all players which never played for the team that drafted them.
 
--- STUB
+CREATE OR REPLACE VIEW those_who_played AS
+SELECT DISTINCT
+    pse.person_id
+FROM
+    drafts dr
+    JOIN player_seasons pse ON pse.person_id = dr.person_id AND pse.team_id = dr.team_id
+;
+
+SELECT
+    p.id, firstname, lastname
+FROM (
+    SELECT DISTINCT person_id FROM drafts
+    MINUS
+    SELECT person_ID FROM those_who_played
+)
+    JOIN people p ON p.id = person_id
+;
+
