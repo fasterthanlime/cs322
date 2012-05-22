6d2489ded328a14cd91a5c29f3ee4444f9a8bbaa
Amos Wenger <amos@official.fm>
2012-05-22 23:26:16 +0200

    Working on query S (#28)

--- queries/basic_s.sql
+++ queries/basic_s.sql
@@ -1,4 +1,26 @@
 -- Compute which was the team with most wins in regular season during which it
 -- changed 2, 3 and 4 coaches.
 
--- STUB
+SELECT
+    team_id, num_coaches, total_wins, year
+FROM (
+    SELECT
+        team_id, num_coaches, total_wins, year,
+        RANK() OVER (PARTITION BY year ORDER BY total_wins DESC) r
+    FROM (
+        SELECT
+            team_id, COUNT(person_id) num_coaches, SUM(season_win) total_wins, year
+        FROM
+            coach_seasons
+        GROUP BY
+            team_id, year
+    )
+    WHERE
+        num_coaches >= 2 AND num_coaches <= 4
+)
+WHERE r = 1;
+
+-- Note that this might give duplicates for ex aequo teams
+-- We can't use the ROWID trick as in Query E, because we
+-- use GROUP BY, so... we'll just be happy with duplicates here.
+
