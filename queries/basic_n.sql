aff88f9efd3a8fcfb61360e3e666b683eca23ecf
sebastien.zurfluh <sebastien.zurfluh@gmail.com>
2012-05-22 19:08:10 +0200

    Query N. closes #23

--- queries/basic_n.sql
+++ queries/basic_n.sql
@@ -3,4 +3,32 @@
 -- 
 -- TENDEX=(points+reb+ass+st+blk‐missedFT‐missedFG‐TO)/minutes
 
--- STUB
+SELECT p.firstname, p.lastname -- TOP 30
+FROM (
+	SELECT p.firstname, p.lastname, (points+reb+asts+st+blk‐missedFT‐missedFG‐to_)/minutes AS tendex
+	FROM (
+		SELECT 
+			p.firstname,
+			p.lastname,
+			SUM(pstats.points) AS points,
+			SUM(pstats.reb) AS reb,
+			SUM(pstats.asts) AS asts,
+			SUM(pstats.steals) AS st,
+			SUM(pstats.blocks) AS blk,
+			SUM(pstats.ftm) AS missedFT,
+			SUM(pstats.fgm) AS missedFG,
+			SUM(pstats.points) AS to_, --TO?
+			SUM(pstats.minutes) AS minutes,
+		FROM
+			people p
+				LEFT OUTER JOIN Players player ON player.person_id = p.id
+				LEFT OUTER JOIN Player_Season pseason ON pseason.player_id = player.id
+				LEFT OUTER JOIN Player_Stats pstats ON pstats.player_season_id = pseason.id
+		GROUP BY p.id
+	)
+	ORDER BY tendex
+)
+WHERE ROWNUM <= 30
+
+
+
