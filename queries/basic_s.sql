48d34721de7d251119692bbcd962b2dd6717eb90
Amos Wenger <amos@official.fm>
2012-05-22 23:44:03 +0200

    Finished view for query S, closes #28

--- queries/basic_s.sql
+++ queries/basic_s.sql
@@ -2,23 +2,29 @@
 -- changed 2, 3 and 4 coaches.
 
 SELECT
-    team_id, num_coaches, total_wins, year
+    team_id, team.name team_name, num_coaches, total_wins, year
 FROM (
     SELECT
-        team_id, num_coaches, total_wins, year,
-        RANK() OVER (PARTITION BY year ORDER BY total_wins DESC) r
+        team_id, num_coaches, total_wins, year
     FROM (
         SELECT
-            team_id, COUNT(person_id) num_coaches, SUM(season_win) total_wins, year
-        FROM
-            coach_seasons
-        GROUP BY
-            team_id, year
+            team_id, num_coaches, total_wins, year,
+            RANK() OVER (PARTITION BY year ORDER BY total_wins DESC) r
+        FROM (
+            SELECT
+                team_id, COUNT(person_id) num_coaches, SUM(season_win) total_wins, year
+            FROM
+                coach_seasons
+            GROUP BY
+                team_id, year
+        )
+        WHERE
+            num_coaches >= 2 AND num_coaches <= 4
     )
-    WHERE
-        num_coaches >= 2 AND num_coaches <= 4
+    WHERE r = 1
 )
-WHERE r = 1;
+    JOIN teams team ON team.id = team_id
+;
 
 -- Note that this might give duplicates for ex aequo teams
 -- We can't use the ROWID trick as in Query E, because we
