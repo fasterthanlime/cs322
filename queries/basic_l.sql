4b05679864d434c26bcc4e3f28d339754c700cd6
Amos Wenger <amos@official.fm>
2012-05-22 16:23:47 +0200

    Query L implemented, closes #21

--- queries/basic_l.sql
+++ queries/basic_l.sql
@@ -1,12 +1,30 @@
 -- List the top 20 career scorers of NBA.
 
-SELECT p.id, p.firstname, p.lastname, pts, r
+CREATE OR REPLACE VIEW nba_players AS
+SELECT
+    pse.person_id, SUM(pst.pts) career_pts
+FROM
+    player_seasons pse
+    JOIN teams t ON t.id = pse.team_id
+    JOIN leagues l ON l.id = t.league_id AND l.name = 'NBA'
+    JOIN player_stats pst ON pst.player_season_id = pse.id
+    JOIN player_season_types pstype ON pstype.id = pst.player_season_type_id AND pstype.name = 'Regular'
+GROUP BY
+    person_id
+;
+
+SELECT
+    person_id, firstname, lastname, career_pts
 FROM (
-    SELECT person_id, pts, row_number() over (ORDER BY pts DESC) r
-    FROM players
-    JOIN player_season_types pst ON pst.id = player_season_type_id
-    WHERE pst.name = 'Regular'
-  ) pl
-  JOIN people p ON p.id = pl.person_id
-WHERE r <= 20
-ORDER BY r ASC;
+    SELECT
+        *
+    FROM
+        nba_players
+    ORDER BY
+        career_pts DESC
+)
+    JOIN people p ON p.id = person_id
+WHERE
+    rownum <= 20
+;
+
