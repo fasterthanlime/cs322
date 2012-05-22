f25302ab805ad54e234bea11423ca6d3d5c34db8
Amos Wenger <amos@official.fm>
2012-05-22 14:07:48 +0200

    Implement query k, closes #20

--- queries/basic_k.sql
+++ queries/basic_k.sql
@@ -1,4 +1,36 @@
 -- List the last and first name of the players who played for a Chicago team
 -- and Houston team.
 
--- STUB
+CREATE OR REPLACE VIEW played_in_chicago AS
+    SELECT
+        person_id
+    FROM
+        player_seasons ps
+        JOIN teams t on ps.team_id = t.id AND t.trigram = 'CHI'
+;
+
+CREATE OR REPLACE VIEW played_in_houston AS
+    SELECT
+        person_id
+    FROM
+        player_seasons ps
+        JOIN teams t on ps.team_id = t.id AND t.trigram = 'HOU'
+;
+
+SELECT
+    p.id, firstname, lastname
+FROM
+    (
+        SELECT
+            *
+        FROM
+            played_in_chicago
+        INTERSECT
+        SELECT
+            *
+        FROM
+            played_in_houston
+    )
+    JOIN people p on p.id = person_id
+;
+
