2229d4561dd731e6746c0e700001073def7da5ac
Yoan Blanc <yoan@dosimple.ch>
2012-05-30 00:17:29 +0200

    Simply, simpler K. #20

--- queries/basic_k.sql
+++ queries/basic_k.sql
@@ -1,38 +1,17 @@
 -- List the last and first name of the players who played for a Chicago team
 -- and Houston team.
 
-CREATE OR REPLACE VIEW played_in_chicago AS
-    SELECT
-        person_id
-    FROM
-        player_seasons ps
-        JOIN teams t on ps.team_id = t.id AND t.trigram IN ('CHI', 'CH1', 'CH2')
-;
-
-CREATE OR REPLACE VIEW played_in_houston AS
-    SELECT
-        person_id
-    FROM
-        player_seasons ps
-        JOIN teams t on ps.team_id = t.id AND t.trigram IN ('HOU', 'HMV')
-;
-
-SELECT
+SELECT DISTINCT
     p.id, firstname, lastname
 FROM
-    (
-        SELECT
-            *
-        FROM
-            played_in_chicago
-        INTERSECT
-        SELECT
-            *
-        FROM
-            played_in_houston
-    )
-    JOIN people p on p.id = person_id
+    player_seasons ps
+    JOIN teams t ON
+        ps.team_id = t.id AND
+        t.trigram IN ('HOU', 'HMV')
+    JOIN player_seasons ps2 ON ps2.person_id = ps.person_id
+    JOIN teams t2 ON
+        ps2.team_id = t2.id AND
+        t2.trigram IN ('CHI', 'CH1', 'CH2')
+    JOIN people p ON p.id = ps.person_id
 ORDER BY
-    lastname, firstname
-;
-
+    lastname, firstname;
