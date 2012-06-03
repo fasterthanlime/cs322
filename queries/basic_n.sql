beb560c57148d135e6f6f4de128264ba9f8098fc
Yoan Blanc <yoan@dosimple.ch>
2012-06-03 19:00:07 +0200

    Fix the stats based on player TENDEX.
    “To answer your question, it depends on the query. If you need the tendex
    of Price as a team member then you need to calculate his TENDEX based on
    his stats for that team, and that team only. If you want his individual
    performance independently of any team for that season then you need to
    use the total stats (which is the top line, which is equal to the sum of
    the other three lines in this example).” — Manos.

--- queries/basic_n.sql
+++ queries/basic_n.sql
@@ -3,25 +3,24 @@
 -- 
 -- TENDEX=(points+reb+asts+stl+blk‐missedFT‐missedFG‐TO)/minutes
 
-CREATE OR REPLACE VIEW player_best_regular_tendices AS
-    SELECT
-        person_id id, MAX(d_tendex) tendex
-    FROM
-        player_seasons ps
-        JOIN player_season_types pst ON pst.id = ps.player_season_type_id
-    WHERE
-        pst.name = 'Regular' AND
-        d_tendex IS NOT NULL
-    GROUP BY
-        person_id;
-
 SELECT
     p.id, firstname, lastname, tendex, r
-FROM
-    (
-        SELECT id, tendex, RANK() OVER (ORDER BY tendex DESC) r
-        FROM player_best_regular_tendices
-        WHERE tendex IS NOT NULL
+FROM (
+        SELECT
+            id, MAX(tendex) tendex,
+            RANK() OVER (ORDER BY MAX(tendex) DESC) r
+        FROM (
+            SELECT person_id id, year, SUM(d_tendex * minutes) / SUM(minutes) tendex
+            FROM
+                player_seasons ps
+                JOIN player_season_types pst ON
+                    pst.id = ps.player_season_type_id AND
+                    pst.name = 'Regular'
+                WHERE d_tendex IS NOT NULL
+                GROUP BY person_id, year
+                ORDER BY tendex DESC
+            )
+        GROUP BY id
     ) t
     JOIN people p ON p.id = t.id
 WHERE r <= 30
--- queries/basic_o.sql
+++ queries/basic_o.sql
@@ -3,25 +3,24 @@
 -- 
 -- TENDEX=(points+reb+ass+st+blk‐missedFT‐missedFG‐TO)/minutes
 
-CREATE OR REPLACE VIEW player_best_playoff_tendices AS
-    SELECT
-        person_id id, MAX(d_tendex) tendex
-    FROM
-        player_seasons ps
-        JOIN player_season_types pst ON pst.id = ps.player_season_type_id
-    WHERE
-        pst.name = 'Playoff' AND
-        d_tendex IS NOT NULL
-    GROUP BY
-        person_id;
-
 SELECT
     p.id, firstname, lastname, tendex, r
-FROM
-    (
-        SELECT id, tendex, RANK() OVER (ORDER BY tendex DESC) r
-        FROM player_best_playoff_tendices
-        WHERE tendex IS NOT NULL
+FROM (
+        SELECT
+            id, MAX(tendex) tendex,
+            RANK() OVER (ORDER BY MAX(tendex) DESC) r
+        FROM (
+            SELECT person_id id, year, SUM(d_tendex * minutes) / SUM(minutes) tendex
+            FROM
+                player_seasons ps
+                JOIN player_season_types pst ON
+                    pst.id = ps.player_season_type_id AND
+                    pst.name = 'Playoff'
+                WHERE d_tendex IS NOT NULL
+                GROUP BY person_id, year
+                ORDER BY tendex DESC
+            )
+        GROUP BY id
     ) t
     JOIN people p ON p.id = t.id
 WHERE r <= 10
