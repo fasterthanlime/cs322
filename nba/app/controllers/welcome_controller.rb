3a6f6bf7e78d92cba16b0be321535b2501008f87
Yoan Blanc <yoan@dosimple.ch>
2012-06-04 08:47:42 +0200

    Super fancy stuff.

--- nba/app/controllers/welcome_controller.rb
+++ nba/app/controllers/welcome_controller.rb
@@ -2,5 +2,12 @@ class WelcomeController < ApplicationController
   def index
     @teams_count = Team.count()
     @people_count = Person.count()
+    query_name = params[:action]
+    rc = ActiveRecord::Base.connection.raw_connection
+    @birthday = rc.exec "
+SELECT id, firstname, lastname, ROUND((sysdate - birthdate) / 364.24)
+FROM people
+WHERE to_char(birthdate, 'mm-dd') = to_char(sysdate, 'mm-dd')
+    "
   end
 end
